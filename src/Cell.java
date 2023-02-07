4581c4289b56e84e64f021a54e1ba578fb7fcdfc
Kay-H7 <kimani712@icloud.com>
2023-02-07 18:01:40 -0500

    "Done Activity"

---
+++ src/Cell.java
@@ -0,0 +1,15 @@
+public class Cell {
+    private String data;
+
+    public void setValue(String cellData){
+        data = cellData;
+    }
+
+    String getData(){
+        return data;
+    }
+
+    public String toString(){
+        return data;
+    }
+}
